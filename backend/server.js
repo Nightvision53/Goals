5c2ec3dda1ae8e44e75ff99c7b6b99d94f0fad14
Babal <babalibrahim34@gmail.com>
2022-02-15 01:15:39 +0300

    Now we have database and be able to do CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const morgan = require('morgan');
 const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+
+//Initialize database
+connectDB();
+
+// Initialize express app
 const app = express();
 
 //Middleware
@@ -15,5 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use('/api/goals', require('./routes/goalRoutes'));
+
 
 app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
