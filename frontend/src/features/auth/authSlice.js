ff02aabf2255a67a6b1c568be82c0aeab67b4330
Babal <babalibrahim34@gmail.com>
2022-02-15 17:45:19 +0300

    Frontend added, Extra reducers for register

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+//Get user from local storage
+const user = JSON.parse(localStorage.getItem('user'));
+
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+
+}
+
+//Register user
+export const register = createAsyncThunk('auth/register',
+    async (user, thunkAPI) => {
+        try {
+            return await authService.register(user);
+        } catch (error) {
+            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+            return thunkAPI.rejectWithValue({ message });
+        }
+    }
+)
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isError = false;
+            state.isSuccess = false;
+            state.isLoading = false;
+            state.message = '';
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(register.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(register.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.message = action.payload.message;
+            state.user = action.payload;
+        });
+        builder.addCase(register.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload.message;
+        }
+        );
+
+    }
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
